0c5490121fc45d3133643d909ad4adfcc7f43d0d
jeppup <jeppup@hotmail.com>
2017-02-11 18:39:44 +0100

    Test commit from cloned version

--- app/src/main/java/com/example/jol/fedora/MainActivity.java
+++ app/src/main/java/com/example/jol/fedora/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //My man
         Intent loginIntent = new Intent(this, LoginActivity.class);
         startActivity(loginIntent);
     }
