fa021b809777d9c3a133dfa8e44e90db20e0e7d7
jeppup <jeppup@hotmail.com>
2017-02-12 17:18:35 +0100

    Added gameActivity

---
+++ app/src/main/java/com/example/jol/fedora/game/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.jol.fedora.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jol.fedora.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
